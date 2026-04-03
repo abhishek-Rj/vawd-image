0c3c334b517d8a35cb2887e704a52de79f149c3d
abhishek-Rj <mahitomaki96@gmail.com>
2026-04-03 16:09:54 +0530

    feat: cors setup

--- vawd_auth/main.go
+++ vawd_auth/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/abhishek-Rj/vawd-image/config"
 	"github.com/abhishek-Rj/vawd-image/database"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,13 @@ func main() {
 	config.Load()
 	database.ConnectionToDB()
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "PUT", "POST", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-type", "Authorization"},	
+		AllowCredentials: true,
+	}))
 
 	server.GET("/", func (c* gin.Context){ 
 		c.JSON(200, gin.H{
