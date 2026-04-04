521d11df3771860be90fbcff774585cd883e8a54
abhishek-Rj <mahitomaki96@gmail.com>
2026-04-04 18:51:38 +0530

    feat: getuserimages

--- vawd_auth/routes/authentication/user/getUserImage.go
+++ vawd_auth/routes/authentication/user/getUserImage.go
@@ -9,10 +9,15 @@ import (
 	"gorm.io/gorm"
 )
 
+type imageDetails struct {
+	Name	string	`json:"name" binding:"required"`
+	Url		string	`json:"url" binding:"required"`
+}
+
 func GetUserImage(c *gin.Context) {
 	userId := c.MustGet("userId")
-
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 7*time.Second)
+	defer cancel()
 
 	images, err := gorm.G[database.Image](database.DB).Where("user_id = ?", userId).Find(ctx)
 	if err != nil {
@@ -20,7 +25,15 @@ func GetUserImage(c *gin.Context) {
 			"error": "Coudn't fetch images",
 		})
 	}
-	response := map[string]interface{}{
-		"images": images,
+
+	var response []imageDetails
+	for _, img := range images {
+		response = append(response, imageDetails{
+			Name: img.Name,
+			Url: img.Url,
+		})
 	}
+	c.JSON(200, gin.H{
+		"images": response,
+	})
 }
